ed1e99b2b7eab7eea19b5dac0e56bbb9cf00785a
vittpol <vittoriaanna.polieri@marconibari.edu.it>
2024-10-01 07:43:49 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+int main() 
+{
+   float risp, costoMoto, totRisp, ammanco, rimanenza;
+   int cout;
+   cout<< "inserisci il costo della moto"
+   cin>>costoMoto;
+   cout<< "inserisci l'ammontare dei tuoi risparmi iniziali";
+   cin>>risp;
+   totRisp=risp
+   count=0
+   while(totRisp<costoMoto)
+   {
+      ammanco=costoMoto-totRisp;
+      cout<<"i risparmi accumulati"<<totRisp<<"non sono sufficenti.Sono necessari altri"<<ammanco;
+      cout<<"quanto altro hai risparmiato?";
+      cin>>risp
+      count=count+1
+      totRisp=totRisp+risp;
+   }
+   cout<<"hai risparmiato"<<totRisp<<"puoicomprare la moto";
+   cout<<"numero di volte che sono stati effettuati risparmi"<<cout;
+   rimanenza=totRisp-costoMoto
+   cout<<"i risparmi rimanenti dopo l' acquisto sono"<<rimanenza;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+}
+    
